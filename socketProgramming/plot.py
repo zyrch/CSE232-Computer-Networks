aca48cb782c4dab50016c7fb52acf7c9562839b9
Parallels <rupanshu19475@iiitd.ac.in>
2021-09-04 22:07:32 +0530

    plot added

---
+++ socketProgramming/plot.py
@@ -0,0 +1,31 @@
+import os
+
+response_time = {}
+
+def get_dic(DIR):
+    times = {}
+    for file in os.listdir(f'{DIR}'):
+        with open(f'{DIR}/{file}', 'r') as f:
+            data = float(f.readline())
+            print(data);
+            clients = int(file[6:file.find('.')])
+            times[clients] = data
+    return times
+
+
+import matplotlib.pyplot as plt
+
+def plot(times):
+    plt.scatter(times.keys(), times.values(), s = 5)
+    plt.xlabel('num processes')
+    plt.ylabel('avg. response time (s)')
+    plt.title('num processes vs response time')
+    plt.show()
+
+plt.savefig("plot.jpg")
+
+if __name__ == "__main__":
+
+    DIR = "response_times"
+    times = get_dic(DIR)
+    plot(times)
